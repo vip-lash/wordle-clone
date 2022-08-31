edc63d35596149215ae635beb95b5eb49223c7d7
vip-lash <79157623+vip-lash@users.noreply.github.com>
2022-08-31 16:51:05 +0530

    added x character

--- src/components/language-configs.js
+++ src/components/language-configs.js
@@ -4,7 +4,7 @@ export default function getLanguageConfigs() {
       keyboard: [
         ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-        ["ENTER", "Z", "C", "V", "B", "N", "M", "BACKSPACE"]
+        ["ENTER", "Z","X", "C", "V", "B", "N", "M", "BACKSPACE"]
       ],
       wordsUrl: "./english.json"
     }
